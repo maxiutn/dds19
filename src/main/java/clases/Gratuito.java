f216d7d5db8dabd3b2211b2c03bc144658287683
patriciomn <pmolinarave@gmail.com>
2019-07-20 21:46:02 -0300

    Borro las clases Gratuito y Premium

--- src/main/java/clases/Gratuito.java
+++
@@ -1,9 +0,0 @@
-package clases;
-
-public class Gratuito extends Usuario{
-
-	public Gratuito(String nombre) {
-		super(nombre);
-		this.tipoDeUsuario = "Gratuito";
-	}
-}
--- src/main/java/clases/Premium.java
+++
@@ -1,9 +0,0 @@
-package clases;
-
-public class Premium extends Usuario{
-
-	public Premium(String nombre) {
-		super(nombre);
-		this.tipoDeUsuario = "Premium";
-	}
-}
